5ccc2e5103b26504e68a516e196a61d18803fed1
Ashish Singh <imrealashu@gmail.com>
2014-11-18 03:12:46 +0530

    Removed unwanted lines, clear and more understandable code

--- server.js
+++ server.js
@@ -3,26 +3,28 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var app = express();
 var bodyParser = require("body-parser");
-var todoSchema = new Schema({
-	title: String,
-	status: Boolean
-});
 app.use("/", express.static(__dirname + '/'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({strict: false}));
 
+var todoSchema = new Schema({
+	title: String,
+	status: Boolean
+});
+
 mongoose.connect('mongodb://localhost/todo_app',function(err, res){
 	if(err){
-		console.log('errror hai');
+		console.log('Connection Error'+err);
 	}else{
-		console.log('ho gya connect');
+		console.log('Connected to Database');
 	}
 });
+
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
-  console.log('yay');
 });
+
 var Todo = mongoose.model('todo',todoSchema);
 
 app.get('/',function(req, res){
@@ -31,9 +33,9 @@ app.get('/',function(req, res){
 
 app.get('/todo/',function(req, res){
 	Todo.find({},{title:1,status:1},'', function (err, todos) {
-  if (err) return handleError(err);
-  res.json(todos);
-});
+		if (err) return handleError(err);
+		res.json(todos);
+	});
 });
 
 app.post('/todo/', function(req, reso){
@@ -42,68 +44,52 @@ app.post('/todo/', function(req, reso){
 	nt.save(function(err, res){
 		if(err){
 			reso.json({"success":false});
-			console.error(err);
+			//console.error(err);
 		}
 		else{
 			console.log(res);
-			//reso.json({"success":true, 'id':res._id});
 			Todo.find({},{title:1,status:1},'', function (err, todos) {
-  if (err) return handleError(err);
-  reso.json(todos);
-});
+				if (err) return handleError(err);
+				reso.json(todos);
+			});
 		}
 	});
-	
-	//console.log(g.data);
 })
 
 app.put('/todo/', function(req, reso){
 	var g = req.body;
-	//var c = new Todo({_id:g._id, 'status':!g.status});
 	var co = {_id: g._id}, up = {$set:{status: !g.status}};
 	Todo.update(co, up, {multi:false}, function(err, res){
-	
 		if(err){
 			reso.json({"success":false});
-			console.error(err);
+			//console.error(err);
 		}
 		else{
-			console.log(res[0]);
 			reso.json({"success":true});
 		}
 	});
-	
-	//console.log(g.data);
 });
 
 app.delete('/todo/:id', function(req, reso){
 	var g = req.params.id;
 	console.log('yahin aya h'+g);
-	//var c = new Todo({_id:g._id});
 	var co = {_id: g};
-	//c.remove();
 	Todo.remove(co, function(err, res){
 		if(err){
 			reso.json({"success":false});
-			console.error(err);
+			//console.error(err);
 		}
 		else{
-			//reso.json({"success":true});
 			Todo.find({},{title:1,status:1},'', function (err, todos) {
-  if (err) return handleError(err);
-  reso.json(todos);
-});
+				if (err) return handleError(err);
+				reso.json(todos);
+			});
 		}
 	});
-	//console.log(g.data);
 });
 
-
 var server = app.listen(3000, function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
-
-})
+	var host = server.address().address
+	var port = server.address().port
+	console.log('Todo App listening at http://%s:%s', host, port)
+});
